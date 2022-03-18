329412e7dd2267385369857b0de7bdef930c0e3d
Andrew Park <andysampark@gmail.com>
2022-03-18 15:44:24 -0500

    changed folders. made dist and ghpages

--- path.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// define('ROOTPATH', realpath(dirname(__FILE__)));
-// define('ROOTPATH', pathinfo(__FILE__, PATHINFO_DIRNAME));
-define('ROOT', '/Applications/XAMPP/xamppfiles/htdocs/green/');
-define('BASE', 'http://localhost/Green/');
-
-?>
-
